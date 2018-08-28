4a82b0da955c91970b821046756580222d8b3724
Cyan <a484599279@hotmail.com>
2018-08-28 18:40:04 +0800

    添加项目文件。

---
+++ MyRoom/AddState.php
@@ -0,0 +1,25 @@
+<?php
+$servername = "localhost";
+$username = "cyan";
+$password = "cyancyan";
+
+// 创建连接
+$conn = new mysqli($servername, $username, $password,"piMonitor");
+// 检测连接
+if ($conn->connect_error) {
+    die("连接失败: " . $conn->connect_error);
+}
+$s = 'insert into RoomState (Temp,Hum,time) VALUES ("'.$_POST["Temp"].'","'.$_POST["Hum"].'","'.date("Y-m-d h:m:s",time()).'");';
+echo $s;
+$result = $conn->query($s);
+
+if($result===TRUE)
+{
+    echo 'OK.';
+}
+else
+{
+    echo $conn->error;
+}
+$conn->close();
+?>
---
+++ PiMonitor/main.cpp
@@ -0,0 +1,7 @@
+#include <cstdio>
+
+int main()
+{
+    printf("hello from PiMonitor!\n");
+    return 0;
+}
